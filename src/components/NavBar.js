52e9f1afa5c272cc912298d69e17821793e849a4
Caroline <bancroftwanjiru10000@gmail.com>
2024-08-06 09:48:15 +0300

    list and keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
+ const linkElements = links.map((link,index)=>{
+  <a key={index} href={"#"+ link}>{link}</a>
+ })
   return (<nav>
-    <a  href="#home">Home</a>,
-    <a href="#about">About</a>,
-    <a href="#projects">Project</a>
+   {linkElements}
     </nav>);
 }
 
